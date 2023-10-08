3f3daa88da67d8f6c13a697d4938bae6760cbbdf
BillClintonAglomasa <aglomasa@yahoo.com>
2023-10-08 16:49:14 +0000

    Making changes to 101 using pointers

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,9 @@ int main(void)
 {
 	const char *part1 = "and that piece of art is useful\" - ";
 	const char *part2 = "Dora Korpar, 2015-10-19\n";
+
 	fputs(part1, stdout);
 	fputs(part2, stdout);
+
 	return (1);
 }
