8c8ef6a848771fa1e977229ec948d2d78eae78c1
BillClintonAglomasa <aglomasa@yahoo.com>
2023-10-17 11:58:37 +0000

    A script containing print_alphabet_x10 function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabets 10 times followed by a line
+ * print_alphabet_x10 - Prints the alphabets 10 times followed by a line
  *
  * Return: Always 0 (Success)
  */
