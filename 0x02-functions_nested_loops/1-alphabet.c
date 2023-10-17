1b20a81448e2d4f3ac236f0bf34ec3356eda8129
BillClintonAglomasa <aglomasa@yahoo.com>
2023-10-17 11:00:20 +0000

    Script containg a function that prints the letters of the alphabets in lower case but returns nothing

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet to the screen
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
