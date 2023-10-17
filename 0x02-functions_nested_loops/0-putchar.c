b4363408c3188a005df80a986ec541d492a1e923
BillClintonAglomasa <aglomasa@yahoo.com>
2023-10-17 10:09:45 +0000

    C program to print the lower and upper cases using putchar only 3 times

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main : Entry point of the program
+ * main - Entry point to program
  *
  * Return: Always return 0 (Success)
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
