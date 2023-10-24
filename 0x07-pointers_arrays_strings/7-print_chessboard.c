b1bd42d788f2b1952fed54c72de54e104af76b6d
BillClintonAglomasa <aglomasa@yahoo.com>
2023-10-24 22:12:08 +0000

    Adding the 7-print_chessboard file containing the print_chessboard functions

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints a chessboard
+ *
+ * @a: Two dimension array that will be printed
+ *
+ * Return: Nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	/* Declaration */
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+
+		_putchar("\n");
+	}
+}
