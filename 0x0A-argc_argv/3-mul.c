45af1d29830c7f0a921811f251c2a40654cf0a5d
BillClintonAglomasa <aglomasa@yahoo.com>
2023-10-30 10:12:36 +0000

    Resolve errors with 3-mul.c and 100-changes.c files

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -61,18 +61,17 @@ int main(int argc, char **argv)
 {
 	int num1, num2, result;
 
-	if (argc == 3)
-	{
-		num1 = _atoi(argv[1]);
-		num2 = _atoi(argv[2]);
-		result = num1 * num2;
-		printf("%d\n", result);
-	}
-	else
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
 
 	return (0);
 }
