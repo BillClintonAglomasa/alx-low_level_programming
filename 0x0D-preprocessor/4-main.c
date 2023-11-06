e38f1b5e6a843d883d2ffd9bc1dccf26ef0ab999
BillClintonAglomasa <aglomasa@yahoo.com>
2023-11-06 22:31:56 +0000

    Adding the 4-sum.h header that contains the SUM(x, y) macros

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "4-sum.h"
+#include "4-sum.h"
+/**
+ * main - Prints the name of the file it was compiled from
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int s;
+
+	s = SUM(98, 1024);
+	printf("%d\n", s);
+
+	return (0);
+}
