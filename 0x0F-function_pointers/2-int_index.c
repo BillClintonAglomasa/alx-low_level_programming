dfdf0de69ccb263408d15596f7da216f773a6df2
BillClintonAglomasa <aglomasa@yahoo.com>
2023-11-09 05:35:25 +0000

    Adding 2-int_index.c and 3-op_functions.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Function searches for an integer
+ *
+ * @array: The array to be iterated over
+ * @size: Size of the array
+ * @action: A pointer to a function
+ *
+ * Return: Return -1 if size <=0 or elem .
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size < 1)
+		return (-1);
+	for (i = 0; i < size, i++)
+	{
+		if (cmp (*(array + x)))
+			return (x);
+	}
+	return (-1);
+}
