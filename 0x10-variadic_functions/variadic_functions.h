837afed67dc9d64df3496dc28e0c19a5adddb3d5
BillClintonAglomasa <aglomasa@yahoo.com>
2023-11-10 00:41:12 +0000

    Adding 0-sum_them_all file summing all the arguments in the variadic function

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,12 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
 
+#include <stdarg.h>
 #include <stdio.h>
 
-int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
-#endif /* FUNCTION_POINTER_H */
+#endif
