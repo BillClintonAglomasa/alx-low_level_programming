37fcb13386f212a7b8efb06e924f0bfe256ec8e6
BillClintonAglomasa <aglomasa@yahoo.com>
2023-11-10 00:27:01 +0000

    creating duplicates of files

---
+++ 0x10-variadic_functions/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - A function that prints a name
+ *
+ * @name: The name to be printed
+ * @f: A pointer to the function that will print name
+ *
+ * Return: Return Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return (1);
+	f(name);
+}
