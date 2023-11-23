e5561ce4be6878e3cf33fe8137b5d2733497b843
BillClintonAglomasa <aglomasa@yahoo.com>
2023-11-23 04:30:47 +0000

    making changes to add_nodedint and add_nodeint_end by changing the sizeof parameter from *newmode to listint_t

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new_node;
 	listint_t *c = *head;
 
-	new_node = malloc(sizeof(*new_node));
+	new_node = malloc(sizeof(listint_t));
 	if (new_node != NULL)
 	{
 		new_node->n = n;
