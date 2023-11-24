29bb272597c03defae976bfb9982229f3d47c972
BillClintonAglomasa <aglomasa@yahoo.com>
2023-11-24 04:27:41 +0000

    Made changes to binary_to_uint and print_binary function and main.h file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,27 +2,6 @@
 #include "main.h"
 
 /**
- * check_valid_string - Checks if a string has only 0's and 1's
- * @b: String to be checked
- *
- * Return: Return 1 if string is valid, 0 otherwise
- */
-int checking_valid_string(const char *b)
-{
-	if (b == NULL)
-		return (0);
-	while (*b)
-	{
-		if (*b != '1' && *b != '0')
-			return (0);
-		b++;
-	}
-
-	return (1);
-}
-
-
-/**
  * binary_to_uint - Converts a binary to an unsigned int
  * @b: The binary number as a string
  *
@@ -30,19 +9,22 @@ int checking_valid_string(const char *b)
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
-	int str_len = 0, base = 1;
+	int a;
+	unsigned int num;
 
-	if (!checking_valid_string(b))
+	num = 0;
+	if (!b)
 		return (0);
-	while (b[str_len] != '\0')
-		str_len++;
-	while (str_len)
+	for (a = 0; b[a] != '\0'; a++)
 	{
-		decimal += ((b[str_len - 1] - '0') * base);
-		base *= 2;
-		str_len--;
+		if (b[a] != '0' && b[a] != '1')
+			return (0);
 	}
-
-	return (decimal);
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		num <<= 1;
+		if (b[a] == '1')
+			num += 1;
+	}
+	return (num);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c)
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-int checking_valid_string(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
